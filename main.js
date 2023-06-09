0ace5844322abb6de014b850210026db72f08ede
MaiteNavas <maitenavas626@gmail.com>
2023-06-09 15:26:20 +0200

    push entrega viernes

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 //ANIMACION MONEDA
+let animationHead = document.getElementById('head-coin');
+let animationTail = document.getElementById('tail-coin');
+let coinContainer = document.getElementById('coins-div');
+
 function animationCoin() {
-    let animationHead = document.getElementById('head-coin');
-    animationHead.classList.add('flip-coin-head');
-    let animationTail = document.getElementById('tail-coin');
-    animationTail.classList.add('flip-coin-tail');
- 
+    coinContainer.classList.add('flip-coin-tail');
+    coinContainer.classList.add('flip-coin-tail');
     setTimeout(function() { 
         animationHead.classList.remove('flip-coin-head');
         animationTail.classList.remove('flip-coin-tail');
@@ -19,26 +20,31 @@ let printHead = document.getElementById('printHead')
 let printTail = document.getElementById('printTail')
     printTail.textContent = "0";
 
+
 //RANDOM CARA O CRUZ
 function randomNumber() {
-    let randomCoin = Math.floor(Math.random() * 2);
+    let randomCoin = Math.floor(Math.random()* 2);
+    coinContainer.style.animation= "none";
     if(randomCoin === 0){
-        printHead.textContent = (`${head++}`);
-        console.log(head);    
-    }
+        setTimeout(function(){
+            printHead.textContent = (`${head++}`);
+            coinContainer.style.animation= "flip-coin-head 3s forwards";
+            console.log(`Cara ${head}`);    
+        },500)}
     else if(randomCoin === 1){
-        printTail.textContent = (`${tail++}`);
-        console.log(tail);
-        }  
+        setTimeout(function(){
+            printTail.textContent = (`${tail++}`);
+            coinContainer.style.animation= "flip-coin-tail 3s forwards";
+            console.log(`Cruz ${tail}`);
+        },500)}
 }
-
 
 //RESETEAR CONTADOR
 function resetGame() {
     tail = 0;
     head = 0;
-        printHead.textContent = (`${head}`);
-        printTail.textContent = (`${tail}`);
-        console.log(tail + head)
-};
+    printHead.textContent = (`${head}`);
+    printTail.textContent = (`${tail}`);
+    console.log(`${tail} + ${head}`);
+}
 
